8f702e367a31091bd93afaa8f92532ddd19b26e8
J0ssue <jdiaz66@icloud.com>
2018-06-27 10:00:21 +0100

    added click functionality and finished refactoring code

--- js/app.js
+++ js/app.js
@@ -1,53 +1,85 @@
-var cat = document.querySelector('#img');
-var clicks = document.querySelector('#clicks');
-clicks.textContent = 0;
-var counter = 0;
-cat.addEventListener('click', function() {
-	counter += 1;
-clicks.textContent = counter;
-alert('Meow!!!');
-}, false);
-
-var cat2 = document.querySelector('#img2');
-var clicks2 = document.querySelector('#clicks2');
-clicks2.textContent = 0;
-var counter2 = 0;
-cat2.addEventListener('click', function() {
-	counter2 += 1;
-	clicks2.textContent = counter2;
-	alert('Meow!!')
-});
-
-
-
-
-// names the cats
-const catNames = function() {
-	// initializes each cat
-	this.init = function() {
-		this.catName(this.cats[0].name, this.cats[0].id);
-		this.catName(this.cats[0].name, this.cats[0].id);
-		this.catName(this.cats[1].name, this.cats[1].id);
-		this.catName(this.cats[1].name, this.cats[1].id);
+// Pattern - Object Literal Module
+let cat = {
+	cats: [
+	{
+		clickCounter: 0,
+		name: 'Grumpy',
+		src: 'src/catClicker.jpg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Happy',
+		src: 'src/chewie.jpg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Cat', 
+		src: 'src/chatty.jpeg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Pedro', 
+		src: 'src/climber.jpeg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Frank', 
+		src: 'src/funk.jpeg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Gold', 
+		src: 'src/gold.jpeg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Sleepy', 
+		src: 'src/sleepy.jpeg'
+	},
+	{
+		clickCounter: 0,
+		name: 'Sunny', 
+		src: 'src/surprise.jpeg'
 	}
-	// stores cats names and id references
-	this.cats = 
-	[
-		{
-			name: 'Grumpy',
-			id: '#cat1'
-		},
-		{
-			name: 'Happy',
-			id: '#cat2'
+	],
+	init: function() {
+		this.cacheDom();
+		this.render();
+		// this.addClick();
+		// this.bindEvents();
+	},
+	cacheDom: function() {
+		this.container = document.querySelector('#cat-container');
+	},
+	// bindEvents: function() {
+	// },
+	render: function() {
+		for (let i = 0; i < this.cats.length; i++) {
+			let newCat = document.createElement('div');
+			this.domCats = `
+											<h2 class="name">${this.cats[i].name}</h2>
+											<h2 class="clickCount">You clicked on <span>${this.cats[i].name}</span> <span id="${this.cats[i].name}">${this.cats[i].clickCounter}</span> times</h2>	
+											<img id="${this.cats[i].name}" class="d-block m-auto" src=${this.cats[i].src} alt="cat picture">
+										  `
+			newCat.innerHTML = this.domCats;
+			this.container.appendChild(newCat);
 		}
-	];
-	// names each cat function
-	this.catName = function(name, catId) {
-		let nameTag = document.querySelector(catId);
-		nameTag.childNodes[1].textContent = name;
-		nameTag.childNodes[3].childNodes[1].textContent = name;
-	};
-	this.init();
+	},
+};
+cat.init();
+
+
+const imgs = document.querySelectorAll('img');
+function addClick() {
+	for (let i = 0; i < imgs.length; i++) {
+		imgs[i].addEventListener('click', function(e) {
+			e.target.previousElementSibling.lastElementChild.textContent++;
+			// for (let j = 0; j < cat.cats.length; j++) {
+			// 	if (e.target.previousElementSibling.lastElementChild.id === cat.cats[j].name) {
+			// 		cat.cats[j].clickCounter++;
+			// 	}
+			// }
+		});
+	}
 }
-catNames();
+addClick();
