009a9e315f6a0dd6c6a8a27ea323a3ca6936f1a3
J0ssue <jdiaz66@icloud.com>
2018-07-08 00:32:00 +0100

    added url object which contains functionality to choose an image url and print it into the image url input.

--- js/app.js
+++ js/app.js
@@ -3,46 +3,55 @@ let model = {
 	setCurrentCat: function() {
 		this.currentCat = this.cats[0];
 	},
+	setCurrentLi: '',
 	cats: [
 	{
 		clickCounter: 0,
-		name: 'Grumpy',
-		src: 'src/catClicker.jpg'
+		name: 'grumpy',
+		src: 'src/catClicker.jpg',
+		id: '0'
 	},
 	{
 		clickCounter: 0,
-		name: 'Happy',
-		src: 'src/chewie.jpg'
+		name: 'happy',
+		src: 'src/chewie.jpg',
+		id: '1'
 	},
 	{
 		clickCounter: 0,
-		name: 'Cat', 
-		src: 'src/chatty.jpeg'
+		name: 'cat', 
+		src: 'src/chatty.jpeg',
+		id: '2'
 	},
 	{
 		clickCounter: 0,
-		name: 'Pedro', 
-		src: 'src/climber.jpeg'
+		name: 'pedro', 
+		src: 'src/climber.jpeg',
+		id: '3'
 	},
 	{
 		clickCounter: 0,
-		name: 'Frank', 
-		src: 'src/funk.jpeg'
+		name: 'frank', 
+		src: 'src/funk.jpeg',
+		id: '4'
 	},
 	{
 		clickCounter: 0,
-		name: 'Gold', 
-		src: 'src/gold.jpeg'
+		name: 'gold', 
+		src: 'src/gold.jpeg',
+		id: '5'
 	},
 	{
 		clickCounter: 0,
-		name: 'Sleepy', 
-		src: 'src/sleepy.jpeg'
+		name: 'sleepy', 
+		src: 'src/sleepy.jpeg',
+		id: '6'
 	},
 	{
 		clickCounter: 0,
-		name: 'Sunny', 
-		src: 'src/surprise.jpeg'
+		name: 'sunny', 
+		src: 'src/surprise.jpeg',
+		id: '7'
 	}
 	]
 };
@@ -54,6 +63,7 @@ let octopus = {
 		listView.init();
 		this.setDefaultCat();
 		adminView.init();
+		displaySrc.init();
 	},
 	setDefaultCat: function() {
 		document.onload = function() {
@@ -66,6 +76,9 @@ let octopus = {
 		for (let i = 0; i < getCats.length; i++) {
 			if (event.target.textContent === getCats[i].name) {
 				model.currentCat = getCats[i];
+				// model.setCurrentLi = event.target;
+				model.setCurrentLi = event.target;
+				console.log(event.target);
 				console.log(model.currentCat);
 			}
 		}
@@ -79,11 +92,21 @@ let octopus = {
 	increaseCounter: function() {
 		let currentCat = this.getCurrentCat();
 		currentCat.clickCounter++;
+	},
+	displaySrcs: function(src) {
+		let cats = this.getCats();		
+		for (let i = 0; i < cats.length; i++) {
+			let el = document.createElement('li');
+			el.innerHTML = cats[i].src;
+			src.appendChild(el);
+		}
 	}
 };
 
 
 // *************  View ***************//
+
+// ************** list
 let listView = {
 	init: function() {
 		this.catList = document.getElementById('catList');
@@ -96,13 +119,16 @@ let listView = {
 		var getCats = octopus.getCats();
 		for (let i = 0; i < getCats.length; i++) {
 			this.li = document.createElement('li');
+			this.li.id = i;
 			this.li.textContent = getCats[i].name;
 			this.catList.appendChild(this.li);
 			this.li.addEventListener('click', octopus.setCurrentCat);
+			this.li.addEventListener('click', octopus.setCurrentLi);
 		}
 	}
 };
 
+// ************** Cat display
 let catView = {
 	init: function() {
 		this.currentCat = octopus.getCurrentCat();
@@ -138,6 +164,7 @@ let catView = {
 	},
 };
 
+// ******************* Admin display
 let adminView = {
 	init: function() {
 		// displaying addming toggle on & off:
@@ -145,25 +172,75 @@ let adminView = {
 		const admin = document.querySelector('#admin');
 		this.displayAdmin(adminBtn, admin);
 
-		// get input values
+		// save Button:
 		const formName = admin.children[0].children[1];
+		const formImg = admin.children[1].children[1];
+		const formClicks = document.querySelector('#form-clicks');;
 		const btnS = admin.children[3].children[0];
-		this.getFormValues(formName, btnS);
+		const btnC = admin.children[3].children[1];
+		this.saveBtn(formName, formImg, formClicks, btnS);
+		this.cancelBtn(btnC);
 	},
 	displayAdmin: function(btn, form) {
 		btn.addEventListener('click', function() {
-			if (form.style.display === 'none') {
-			form.style.display = 'block';
+			if (form.style.display === 'block') {
+			form.style.display = 'none';
 			} else {
-				form.style.display = 'none';
+				form.style.display = 'block';
 			}
 		});
 	},
-	getFormValues: function(name, btnS) {
-		btnS.addEventListener('click', function() {
-			console.log(name.value)
+	saveBtn: function(name, img, clicks, btn) {
+		const ul = document.querySelector('#catList');
+		let currentCat = octopus.getCurrentCat();
+		let cats = octopus.getCats();
+
+		btn.addEventListener('click', function() {
+			for (let i = 0; i < ul.childNodes.length; i++) {
+				if (ul.children[i].id == currentCat.id) {
+					// cats[i].name = name.value;	
+					currentCat.name = name.value;
+					currentCat.clickCounter = clicks.value;
+					currentCat.src = img.value;
+					model.setCurrentLi.textContent = name.value;
+
+					name.value = ""; 
+					clicks.value = ""; 
+					img.value = ""; 
+				}
+			}
+		});
+	},
+	cancelBtn: function(btn) {
+		btn.addEventListener('click', function() {
+			let clicks = document.querySelector('#form-clicks');
+			let img = document.querySelector('#form-img');
+			let name = document.querySelector('#form-name');
+			clicks.value = '';
+			img.value = '';
 			name.value = '';
 		});
+	}
+};
+
+let displaySrc = {
+	init: function() {
+		this.displaySrcs();
+		this.addToInput();
+	},
+	displaySrcs: function() {
+		let srcsList = document.querySelector('#urls');
+		octopus.displaySrcs(srcsList);
+	},
+	addToInput: function() {
+		let srcsList = document.querySelector('#urls');
+		let srcInput = document.querySelector('#form-img');
+		for (let i = 0; i < srcsList.children.length; i++) {
+			srcsList.children[i].addEventListener('click', function(e) {
+				srcInput.value = e.target.textContent;
+				console.log(e.target);
+			});
+		}
 	}
 };
 
